b368deb3f9ffa552924f045e0a880348c3c5f98f
thisisDom <Dominique.Crowther@gmail.com>
2017-07-02 21:47:32 -0700

    adds association to player and player’s level

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,4 +1,6 @@
 class Level < ApplicationRecord
+  has_many :levelsresources
+  has_many :resources, { through: :levelsresources }
 
   validates :assets, { presence: true }
   validates :position, { presence: true,
