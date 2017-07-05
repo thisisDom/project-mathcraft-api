e35f98b34b4f9227de28e5687b0c879f3b03c024
thisisDom <Dominique.Crowther@gmail.com>
2017-07-05 15:19:43 -0700

    fixes error in application config file

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,6 @@ module MathcraftApi
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.api_only = true
-    configs.autoload_paths << Rails.root.join("lib")
+    config.autoload_paths << Rails.root.join("lib")
   end
 end
