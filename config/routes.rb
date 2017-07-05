b8fc422c6cc819302931f2f3ce03b979d6ff2328
thisisDom <Dominique.Crowther@gmail.com>
2017-07-05 15:19:06 -0700

    fixes error in route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # api token authentication
-  post 'authenticate', to 'authentication#authenticate'
+  post '/authenticate', to: 'authentication#authenticate'
 
 end
