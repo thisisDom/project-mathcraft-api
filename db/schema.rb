706c8b3753fb84c309e9f55b0973314374d03729
thisisDom <Dominique.Crowther@gmail.com>
2017-07-05 19:19:47 -0700

    adds table for Registered App model to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170703050854) do
+ActiveRecord::Schema.define(version: 20170705180854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,14 @@ ActiveRecord::Schema.define(version: 20170703050854) do
     t.datetime "updated_at", null: false
     t.index ["player_id"], name: "index_players_resources_on_player_id"
     t.index ["resource_id"], name: "index_players_resources_on_resource_id"
+  end
+
+  create_table "registered_apps", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_registered_apps_on_email_address", unique: true
   end
 
   create_table "resources", force: :cascade do |t|
