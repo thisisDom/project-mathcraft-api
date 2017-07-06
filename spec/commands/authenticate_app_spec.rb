325c4485f0f2b679fc6f54f81bd99d825b995935
thisisDom <Dominique.Crowther@gmail.com>
2017-07-05 19:19:03 -0700

    creates spec test for the AuthorizeApp command model

---
+++ spec/commands/authenticate_app_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe AuthenticateApp, type: :command do
+  it 'can set a JSON Web Token for an authorized app' do
+    email, password = 'test1','test1'
+    user = RegisteredApp.create({email_address: email, password: password})
+    command = AuthenticateApp.call(email, password)
+    expect(command.success?).to eq true
+    p command.result
+  end
+
+  it 'returns an error if app is not authorized' do
+    command = AuthenticateApp.call('test2', 'test2')
+    expect(command.success?).to eq false
+  end
+end
