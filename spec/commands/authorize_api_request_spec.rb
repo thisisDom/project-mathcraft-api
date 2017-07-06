3d2a86c5a526b6fc12ad80a9e2f81cfef63dd6fd
thisisDom <Dominique.Crowther@gmail.com>
2017-07-06 07:04:33 -0700

    adds test for not authorized api request

--- spec/commands/authorize_api_request_spec.rb
+++ spec/commands/authorize_api_request_spec.rb
@@ -1,10 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe AuthorizeApiRequest, type: :command do
-    it 'authorizes api request' do
-      RegisteredApp.create({email_address: 'test', password: 'test'})
-      token = AuthenticateApp.call('test', 'test').result
-      response = AuthorizeApiRequest.call({ 'Authorization' => "Basic #{token}" })
-      expect(response).to be_truthy
+  describe 'api request' do
+      context 'authorized if' do
+        it 'has a valid token' do
+          RegisteredApp.create({email_address: 'test', password: 'test'})
+          token = AuthenticateApp.call('test', 'test').result
+          response = AuthorizeApiRequest.call({ 'Authorization' => "Basic #{token}" })
+          expect(response).to be_truthy
+        end
+      end
+
+      context 'not authorized if' do
+        it 'token is not present' do
+          RegisteredApp.create({email_address: 'test', password: 'test'})
+          token = AuthenticateApp.call('test', 'test').result
+          response = AuthorizeApiRequest.call()
+          expect(response.errors[:token][0]).to eq 'Missing Token'
+        end
+
+        it 'token is invalid' do
+          token = 'test'
+          response = AuthorizeApiRequest.call({ 'Authorization' => "Basic #{token}" })
+          expect(response.errors[:token][0]).to eq 'Invalid Token'
+        end
+      end
     end
 end
