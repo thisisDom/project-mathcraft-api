520bf9c1158ae378b36eaa379d636927877f0bbd
thisisDom <Dominique.Crowther@gmail.com>
2017-07-08 23:50:31 -0700

    adds methods to Player level to getting time for a level attempt and adding resources to the players inventory based on time

--- app/models/players_level.rb
+++ app/models/players_level.rb
@@ -3,4 +3,31 @@ class PlayersLevel < ApplicationRecord
   belongs_to :level
 
   validates :time_started, :time_completed, { presence: true }
+
+  def time
+    self.time_completed - self.time_started
+  end
+
+  def add_resources
+    levels_resources = LevelsResources.find_by(level_id: self.level_id)
+
+    return false if levels_resources.lenght == 0
+
+    level_resources.each { |level_resource|
+      player_resource = PlayersResource.where(player_id: self.player_id, resource_id: level_resource.resource_id).first_or_create
+
+      resource_gain = level_resource.rate * self.time
+
+      if resource_gain > level_resource.max_quantity
+        resource_gain = level_resource.max_quantity
+      elsif resource_gain < level_resource.min_quantity
+        resource_gain = level_resource.min_quantity
+      end
+
+      new_resource = player_resource.quantity + resource_gain
+      player_resource.update(quantity: new_resource)
+    }
+    true
+  end
+
 end
