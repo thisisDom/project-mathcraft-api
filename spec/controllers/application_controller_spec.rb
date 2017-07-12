3927c260fb26bd4c94db3017e71cbb32324f6d46
thisisDom <Dominique.Crowther@gmail.com>
2017-07-11 17:43:47 -0700

    comments out token authentication

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,17 +1,10 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe ApplicationController, type: :controller do
-#   before(:all) do
-#     RegisteredApp.create({email_address: 'project-mathcraft', password: 'fishsticks'})
-#   end
-#
-#   describe 'authorization of requests' do
-#     context 'authorized' do
-#       it 'responds with the registered app that made the request' do
-#         post :authenticate, params: { email_address: 'project-mathcraft', password: 'fishsticks' }
-#         token = response.body
-#         request.headers['Authentication'] = "Basic #{token}"
-#       end
-#     end
-#   end
-# end
+xRSpec.describe ApplicationController, type: :controller do
+  before(:all) do
+    RegisteredApp.create({email_address: 'project-mathcraft', password: 'fishsticks'})
+    post :authenticate, params: { email_address: 'project-mathcraft', password: 'fishsticks' }
+    token = response.body
+    request.headers['Authentication'] = "Basic #{token}"
+  end
+end
--- spec/controllers/authorization_controller_spec.rb
+++ spec/controllers/authorization_controller_spec.rb
@@ -1,31 +1,32 @@
-# require 'rails_helper'
-#
-# RSpec.describe AuthorizationController, type: :controller do
-#   before(:all) do
-#     RegisteredApp.create({email_address: 'project-mathcraft', password: 'fishsticks'})
-#   end
-#
-#   describe 'POST #authenticate' do
-#     context 'Authorized' do
-#       before(:each) do
-#         post :authenticate, params: {email_address: 'project-mathcraft', password: 'fishsticks' }
-#       end
-#       it 'responds with status code 200 if authorized' do
-#         expect(response.status).to eq 200
-#       end
-#       it 'responds with API token' do
-#         expect(response.content_type).to eq "application/json"
-#         expect(response.body).to include('auth_token')
-#       end
-#     end
-#
-#     context 'Not Authorized' do
-#       it 'responds with 401 if no credentials are present' do
-#         post :authenticate
-#         expect(response.status).to eq 401
-#         expect(response.body).to include("user_authentication")
-#       end
-#     end
-#   end
-#
-# end
+require 'rails_helper'
+
+xRSpec.describe AuthorizationController, type: :controller do
+  before(:all) do
+    RegisteredApp.create({email_address: 'project-mathcraft', password: 'fishsticks'})
+  end
+
+  describe 'POST #authenticate' do
+    context 'Authorized' do
+      before(:each) do
+        post :authenticate, params: {email_address: 'project-mathcraft', password: 'fishsticks' }
+      end
+      it 'responds with status code 200 if authorized' do
+        expect(response.status).to eq 200
+      end
+      it 'responds with API token' do
+        expect(response.content_type).to eq "application/json"
+        expect(response.body).to include('auth_token')
+      end
+    end
+
+    context 'Not Authorized' do
+      it 'responds with 401 if no credentials are present' do
+        post :authenticate
+        expect(response.status).to eq 401
+        expect(response.body).to include("user_authentication")
+      end
+      it 'responds with 401 if token is invalid'
+    end
+  end
+
+end
