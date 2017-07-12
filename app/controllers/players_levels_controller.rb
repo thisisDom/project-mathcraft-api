05b59cbb717794ae860a0acaeb5b7f482f0f2c3f
thisisDom <Dominique.Crowther@gmail.com>
2017-07-12 15:38:32 -0700

    fixes error

--- app/controllers/players_levels_controller.rb
+++ app/controllers/players_levels_controller.rb
@@ -11,27 +11,30 @@ class PlayersLevelsController < ApplicationController
         render json: { errors: players_level.errors.full_messages }, status: 500
       end
     else
-      render json: { errors: 'Invalid Level or Player' }, status: 404
+      render json: { errors: 'Invalid Level Or Player' }, status: 404
     end
   end
 
   def complete
-    players_level = PlayersLevel.find_by(id: players_level_params[:player_id])
+    players_level = PlayersLevel.find_by(id: params[:id])
     if players_level
-      players_level.update(time_completed: Time.current.to_i)
-      players_level.add_resources
-      players_level.player.level_up(players_level.level.experience)
-
-      render json: { player: player.as_json(methods: [:buildings, :resources, :level]) }
+      if players_level.player_id == players_level_params[:player_id].to_i
+        players_level.update({time_completed: Time.current, correct_answers: players_level_params[:correct_answers]})
+        players_level.add_resources
+        players_level.player.level_up(players_level.level.experience)
+        render json: { player: players_level.player.as_json(methods: [:level, :buildings, :resources]) }, status: 202
+      else
+        render json: { errors: 'Not Authorized' }, status: 402
+      end
     else
-      render json: { errors: players_level.errors.full_messages }
+      render json: { errors: 'Level Attempt Not Found' }, status: 404
     end
   end
 
   private
 
   def players_level_params
-    params.require(:data).permit(:level_id, :player_id)
+    params.require(:data).permit(:level_id, :player_id, :correct_answers)
   end
 
 end
