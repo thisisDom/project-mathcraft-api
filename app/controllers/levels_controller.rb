802bdbf3279d4345f7f1edac36fe3ecbe643dbda
thisisDom <Dominique.Crowther@gmail.com>
2017-07-12 09:19:09 -0700

    adds json conversion

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -1,7 +1,7 @@
 class LevelsController < ApplicationController
 
   def index
-    render json: { levels: Level.order(:position) }
+    render json: { levels: Level.order(:position).as_json }
   end
 
 end
