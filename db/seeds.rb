cadf0a3d30237d5b1d5c9224664dc6f3700fa3d7
thisisDom <Dominique.Crowther@gmail.com>
2017-07-12 09:38:05 -0700

    adds seed demo player for resources and levels

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,42 +13,52 @@ while Experience.count < max_level
   Experience.create({ level: Experience.count, experience_needed: Experience.count ** 3 })
 end
 
-#create levels
-Level.create({ assets: { "phaser-background" => "forest.png",
-                         "calculator-background" => "forest.png",
-                         "minions" => ["stumpy.png"],
-                         "boss" => "hard_mode"
-                       },
-                position: 1,
-                experience: 10,
-                time_limit: 30,
-                level_requirement: 0,
-                title: 'forest',
-                level_type: 'time_attack'
-              })
+# #create levels
+# Level.create({ assets: { "phaser-background" => "forest.png",
+#                          "calculator-background" => "forest.png",
+#                          "minions" => ["stumpy.png"],
+#                          "boss" => "hard_mode"
+#                        },
+#                 position: 1,
+#                 experience: 10,
+#                 time_limit: 30,
+#                 level_requirement: 0,
+#                 title: 'forest',
+#                 level_type: 'time_attack'
+#               })
+#
+# Level.create({ assets: { "phaser-background" => "cave.png",
+#                          "calculator-background" => "cave.pngg",
+#                          "minions" => ["golem.png"],
+#                          "boss" => "hard_mode"
+#                         },
+#                position: 2,
+#                experience: 20,
+#                time_limit: 30,
+#                level_requirement: 1,
+#                title: 'cave',
+#                level_type: 'time_attack'
+#               })
+#
+# Level.create({ assets: { "phaser-background" => "temple.jpg",
+#                           "calculator-background" => "temple.jpg",
+#                           "minions" => [],
+#                           "boss" => { "alive": "boss-stomp.png", "death": "boss-death.png"}
+#                         },
+#                position: 3,
+#                experience: 30,
+#                level_requirement: 2,
+#                time_limit: 30,
+#                title: 'temple',
+#                level_type: 'boss_battle'
+#               })
 
-Level.create({ assets: { "phaser-background" => "cave.png",
-                         "calculator-background" => "cave.pngg",
-                         "minions" => ["golem.png"],
-                         "boss" => "hard_mode"
-                        },
-               position: 2,
-               experience: 20,
-               time_limit: 30,
-               level_requirement: 1,
-               title: 'cave',
-               level_type: 'time_attack'
-              })
+player = Player.create({username: "Jasper", email_address: 'test', password: 'test', avatar_asset_link: 'test' })
 
-Level.create({ assets: { "phaser-background" => "temple.jpg",
-                          "calculator-background" => "temple.jpg",
-                          "minions" => [],
-                          "boss" => { "alive": "boss-stomp.png", "death": "boss-death.png"}
-                        },
-               position: 3,
-               experience: 30,
-               level_requirement: 2,
-               time_limit: 30,
-               title: 'temple',
-               level_type: 'boss_battle'
-              })
+r1 = Resource.create(name: "wood", asset_link: 'wood.png')
+r2 = Resource.create(name: "stone", asset_link: 'stone.png')
+r3 = Resource.create(name: "gold", asset_link: 'gold.png')
+
+PlayersResource.create({player: player, resource: r1, quantity: 10})
+PlayersResource.create({player: player, resource: r2, quantity: 5})
+PlayersResource.create({player: player, resource: r3, quantity: 69})
