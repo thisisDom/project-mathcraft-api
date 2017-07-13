adf496ae82f073cac36f1f016117c00b77cb7946
thisisDom <Dominique.Crowther@gmail.com>
2017-07-12 17:47:54 -0700

    creates seed for buildings

--- db/seeds.rb
+++ db/seeds.rb
@@ -57,12 +57,40 @@ Level.create({ assets: { "phaser-background" => "temple.jpg",
 end
 
 
-player = Player.create({username: "Jasper", email_address: 'test', password: 'test', avatar_asset_link: 'test' })
-
 r1 = Resource.create(name: "wood", asset_link: 'wood.png')
 r2 = Resource.create(name: "stone", asset_link: 'stone.png')
 r3 = Resource.create(name: "gold", asset_link: 'gold.png')
 
-PlayersResource.create({player_id: 1, resource_id: 1, quantity: 10})
-PlayersResource.create({player_id: 1, resource_id: 2, quantity: 5})
-PlayersResource.create({player_id: 1, resource_id: 3, quantity: 69})
+building = Building.create({name: 'alchemy-lab-1', asset_link: 'alchemy-lab-1.png', building_level: 1, building_class: "Alchemy Lab", offsetX: -80, offsetY: -10 })
+BuildingsResource.create({resource: r2, building: building, quantity: 3})
+
+building = Building.create({name: 'alchemy-lab-2', asset_link: 'alchemy-lab-2.png', building_level: 2, building_class: "Alchemy Lab", offsetX: -120, offsetY: -30 })
+BuildingsResource.create({resource: r2, building: building, quantity: 5})
+
+building = Building.create({name: 'alchemy-lab-3', asset_link: 'alchemy-lab-3.png', building_level: 3, building_class: "Alchemy Lab", offsetX: -150, offsetY: -60})
+BuildingsResource.create({resource: r2, building: building, quantity: 10})
+
+building = Building.create({name: 'tesla-factory-1', asset_link: 'tesla-factory-1.png', building_level: 1, building_class: "Tesla Factory", offsetX: -90, offsetY: 0 })
+BuildingsResource.create({resource: r1, building: building, quantity: 5})
+
+building = Building.create({name: 'tesla-factory-2', asset_link: 'tesla-factory-2.png', building_level: 2, building_class: "Tesla Factory", offsetX: -140, offsetY: -60 })
+BuildingsResource.create({resource: r1, building: building, quantity: 5})
+
+building = Building.create({name: 'tesla-factory-3', asset_link: 'tesla-factory-3.png', building_level: 3, building_class: "Tesla Factory", offsetX: -160, offsetY: -70})
+BuildingsResource.create({resource: r1, building: building, quantity: 10})
+
+building = Building.create({name: 'magic-house-1', asset_link: 'magic-house-1.png', building_level: 1, building_class: "Magic House", offsetX: -90, offsetY: -10 })
+
+BuildingsResource.create({resource: r3, building: building, quantity: 2})
+
+building = Building.create({name: 'magic-house-2', asset_link: 'magic-house-2.png', building_level: 2, building_class: "Magic House", offsetX: -90, offsetY: -20 })
+
+BuildingsResource.create({resource: r3, building: building, quantity: 5})
+
+building = Building.create({name: 'magic-house-3', asset_link: 'magic-house-3.png', building_level: 3, building_class: "Magic House", offsetX: -90, offsetY: -20}
+BuildingsResource.create({resource: r3, building: building, quantity: 10})
+
+
+
+
+player = Player.create({username: "Jasper", email_address: 'test', password: 'test', avatar_asset_link: 'test' })
